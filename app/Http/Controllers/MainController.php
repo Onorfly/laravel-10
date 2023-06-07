0e61537c1498fffc4229fc379e72d05655120a64
Robert Naumetov <naurobert@yandex.ru>
2023-06-07 11:57:11 +0400

    master blade

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
@@ -11,14 +12,24 @@ class MainController extends Controller
     }
 
     public function categories() {
-        return view('categories');
+        $categories = Category::get();
+        return view('categories', compact('categories'));
     }
 
-    public function category($category) {
-        dd($category);
+    public function category($code) {
+        $category = Category::where('code', $code)->first();
+        return view('category', compact('category'));
     }
 
     public function product($product = null) {
         return view('product', ['product' => $product]);
+    }
+
+    public function basket() {
+        return view('basket');
+    }
+
+    public function basketPlace() {
+        return view('order');
     }
 }
